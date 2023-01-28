aa02b8188618dd5aee2c18b4d5fcb62142dcf9b9
andiK19 <andreas.kremling@outlook.com>
2023-01-28 10:19:50 +0100

    etting spaces to borders

--- MemeMe/Cell/MemeTableViewCell.swift
+++ MemeMe/Cell/MemeTableViewCell.swift
@@ -9,7 +9,15 @@ import UIKit
 
 class MemeTableViewCell: UITableViewCell {
 
-//Outlets
+    //Outlets
     @IBOutlet var mImage: UIImageView!
     @IBOutlet var mName: UILabel!
+    
+    //setting spaces to borders
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        //set the values for top,left,bottom,right margins
+                let margins = UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0)
+        contentView.frame = contentView.frame.inset(by: margins)
+    }
 }
